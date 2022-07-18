1c8535da852bc492bfaccc8e833611197d480f3c
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-07-18 12:52:10 -0300

    connection to database with sequelize

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const config = require('./config');
-
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const router = require('./routes');
 
 
-app.use(router);
+app.use('/', router);
+
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 
 app.listen(config.port, () => {
-    console.log(`Servewr running on port ${config.port}`);
+    console.log(`Servewr running on http://localhost:${config.port}`);
 });
---
+++ src/sequelize/index.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require('sequelize');
+const config = require('../config');
+
+const sequelize = new Sequelize(config.db_name, config.db_user, config.db_password, {
+    hostname: 'localhost',
+    dialect: 'mysql'
+});
+
+const connectToDatabase = () => {
+    sequelize.authenticate()
+    .then( () => {
+        console.log(`Connection to database ${config.db_name} has been established successfully`);
+    })
+    .catch( error => {
+        console.log('Unable to connect to database', error);
+    });
+};
+
+connectToDatabase();
+
+module.exports = sequelize;
