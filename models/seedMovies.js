00ee97dfc0663d6953dbf595fc89685661e5d23d
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-08-09 00:28:59 -0300

    seed movies

---
+++ models/seedMovies.js
@@ -0,0 +1,33 @@
+const { Movie } = require('./index');
+
+const newMovies = async () => {
+    await Movie.create({
+        image:'https://movie.com/steamboat-willie.jpg',
+        title:'Steamboat Willie',
+        date:'1928',
+        rate: 10,
+        genreId: 1,
+        createdAt: Date.now(),
+        updatedAT: Date.now()
+    });
+    await Movie.create({
+        image:'https://movie.com/plane-crazy.jpg',
+        title:'Plane Crazy',
+        date:'1928',
+        rate: 7,
+        genreId: 1,
+        createdAt: Date.now(),
+        updatedAT: Date.now()
+    });
+    await Movie.create({
+        image:'https://movie.com/christmas.jpg',
+        title:'Mickeys Twice Upon a Christmas',
+        date:'2004',
+        rate: 6,
+        genreId: 2,
+        createdAt: Date.now(),
+        updatedAT: Date.now()
+    });
+};
+
+newMovies();
