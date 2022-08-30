d6c46e5837ec4922505511e9c76162380362c658
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-08-29 22:26:35 -0300

    users repo

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Role } = require('../models');
 
 
 const createNewUserRepo = async (user) => {
@@ -6,4 +6,57 @@ const createNewUserRepo = async (user) => {
 };
 
 
-module.exports = { createNewUserRepo };
+const getAllUsersRepo = async () => {
+    return await User.findAll({
+        include: {
+            model: Role
+        }
+    });
+};
+
+
+const getUserByEmailRepo = async (email) => {
+    return await User.findOne({
+        where: {
+            email: email
+        },
+        include: [
+            {
+                model: Role
+            }
+        ]
+    },
+    );
+};
+
+
+const getUserByIdRepo = async (id) => {
+    return await User.findByPk(id, {
+        include: [
+            {
+                model: Role
+            }
+        ]
+    });
+};
+
+
+const updateUserRepo = async (user, id) => {
+    return await User.update(user, {
+        where: {
+            id
+        }
+    });
+};
+
+
+const deleteUserRepo = async (id) => {
+ return await User.destroy({
+    where: {
+        id
+    }
+ })
+};
+
+
+module.exports = { createNewUserRepo, getAllUsersRepo, getUserByEmailRepo, getUserByIdRepo, updateUserRepo, deleteUserRepo };
