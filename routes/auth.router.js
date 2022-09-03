04d40490d4bbd76dc029ed76498efecd1bc9d542
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-09-03 20:41:38 -0300

    user check to verify if valid user accesing endpoint

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,12 +1,21 @@
 const router = require('express').Router();
-const { userRegister, userLogin, getAllUsers, updateUser, updateUserPassword, deleteUser } = require('../controllers/users.controller');
 const { isAdmin } = require('../middlewares/isAdmin');
 const { isOwnerOrAdmin } = require('../middlewares/isOwnerOrAdmin');
+const {
+    getAllUsers,
+    userLogin,
+    userRegister,
+    userCheck,
+    updateUser,
+    updateUserPassword,
+    deleteUser
+} = require('../controllers/users.controller');
 
 
 router.get('/all', isAdmin, getAllUsers);
 router.post('/login', userLogin);
 router.post('/register', userRegister);
+router.post('/check', userCheck);
 router.put('/userupdate', isOwnerOrAdmin, updateUser);
 router.put('/updatepassword', isOwnerOrAdmin, updateUserPassword);
 router.delete('/:id', isOwnerOrAdmin, deleteUser);
