2d4863a2ff86910a0487ea4d8b9108e4ceb6f14a
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-10-19 03:41:23 -0300

    swagger initialization and json doc

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const routes = require('./routes');
 const port = config.server_port;
 const path = require('path');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./documentation/swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
